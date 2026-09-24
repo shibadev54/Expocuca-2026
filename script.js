210b2404b9447b4a3fa528925f76075ebd728b5f
shibadev54 <juliobragadelima@gmail.com>
2026-09-23 22:59:47 -0300

    adição de modulos e correção de layout

--- script.js
+++ script.js
@@ -1,24 +1,5 @@
+const darkModeButton = document.getElementById("darkMode");
 
-        const darkModeButton =
-            document.getElementById("darkMode");
-
-        darkModeButton.addEventListener("click", () => {
-
-            document.body.classList.toggle("dark");
-
-            const darkModeAtivo =
-                document.body.classList.contains("dark");
-
-            if (darkModeAtivo) {
-
-                darkModeButton.textContent =
-                    "☀️";
-
-            } else {
-
-                darkModeButton.textContent =
-                    "🌙";
-
-            }
-
-        });
+darkModeButton.addEventListener("click", () => {
+    document.body.classList.toggle("dark");
+});
